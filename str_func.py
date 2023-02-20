b5d01ea69d29ff1ba1127fe16ecd8869044558a6
Korolev_Viktor <Lekantrop74@yandex.ru>
2023-02-20 17:15:48 +0500

    add  func "cap" and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def up(input_srt: str) -> str:
+    """
+    add docstring
+    """
     return input_srt.upper()
+
+def cap(input_str: str) ->str:
+    """
+    add  docstring_2
+    """
+    return input_str.capitalize()
